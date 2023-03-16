8b244a0fdd54ae1578292aa2bfa7413407e17262
DjGrib <65597861+DjGrib@users.noreply.github.com>
2023-03-16 23:54:17 +0300

    условие 66

--- HWIP/Program.cs
+++ HWIP/Program.cs
@@ -1,4 +1,5 @@
-﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+﻿// Задача 64: 
+// Задайте значение N. Напишите программу, которая выведет 
 // все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
 // N = 5 -> "5, 4, 3, 2, 1"
@@ -26,3 +27,29 @@
 //    }
 
 //Console.WriteLine(NaturalNumb(n, 1));
+
+
+// Задача 66: 
+// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Prompt(string message)
+    {
+        Console.Write(message);
+        int result = Convert.ToInt32(Console.ReadLine());
+        return result;
+    }
+
+int SumOfNumb(int n, int m)
+    {
+        if (n == m) return n;
+        else return SumOfNumb(n +1, m) + n;
+    }
+
+int n = Prompt("Введите первое число:");
+int n = Prompt("Введите второе число:");
+
+Console.WriteLine(SumOfNumb(n, m));
